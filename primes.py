6b86d9b64305e18f95d1b1a5a97a4f722bb91041
Anna1021 <k22008684@kcl.ac.uk>
2023-11-10 15:02:46 +0000

    implemented the fuction

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    primes_list = []
+    num = 2  # Starting from the first prime number
+    if number_of_primes <=0:
+        raise ValueError('input number must be positive')
+    while len(primes_list) < number_of_primes:
+        is_prime = all(num % i != 0 for i in range(2, int(num**0.5) + 1))
+        if is_prime:
+            primes_list.append(num)
+        num += 1
+
+    return primes_list
+
+# Test the function with number_of_primes = 10
+result = primes(10)
+print(result)
+
+
+
